3593405272e33036ef836be6ac1ad3ea372b389a
Gorkem Yontem <gorkem@evtiko.com>
2017-01-07 19:50:03 +0300

    remove specification plugin

--- content/themes/rehub-vendor/stardomcity-base.php
+++ content/themes/rehub-vendor/stardomcity-base.php
@@ -6,34 +6,126 @@ class StardomCityBase {
     }
 
     private function init(){
-      add_filter( 'product_type_options', array( $this, 'custom_product_type_options' ), 10);
-      add_filter('product_type_selector', array( $this, 'custom_product_type_selector'), 10, 2);
-      add_filter('product_type_options', array( $this, 'custom_product_type_options'), 10);
       //   add_filter( 'login_redirect', array( $this, 'custom_user_login_redirect' ), 10, 3 );  //static
+      add_filter( 'wcv_product_description', array( $this, 'make_required_fieldname' ), 10, 1);
 
       // add_filter( 'media_upload_newtab', array( $this, 'media_upload_callback' ) );  //instance
       // add_filter( 'media_upload_newtab', array( 'My_Class', 'media_upload_callback' ) );  //static
       // add_filter( 'the_title', function( $title ) { return '<strong>' . $title . '</strong>'; } ); //anonymous
     }
 
-  // function custom_product_type_options( $product_type_options ) {
-  // 	  $product_type_options['virtual'][ 'default' ] = 'yes';
-  // 	  return $product_type_options;
-  // }
 
-  //Removes 'simple'   => __( 'Simple product', 'wcvendors-pro' ) ,  //'grouped'  => __( 'Grouped product', 'wcvendors-pro' ),	//external' => __( 'External/Affiliate product', 'wcvendors-pro' ),   //'variable' => __( 'Variable product', 'wcvendors-pro' ),
-  function custom_product_type_selector($a, $b) {
+function make_required_fieldname( $args ) {
+  return $args;
+  // 'product_type_options', array(
+  //  'virtual' => array(
+  //    'id'            => '_virtual',
+  //    'wrapper_class' => 'show_if_simple',
+  //    'label'         => __( 'Virtual', 'wcvendors-pro' ),
+  //    'description'   => __( 'Virtual products are intangible and aren\'t shipped.', 'wcvendors-pro' ),
+  //    'default'       => 'no'
+  //  ),
 
-  	$product_type = array();
-  	return $product_type;
-  }
 
-  function custom_product_type_options() {
-  	$product_type_options = array(
-  			//'virtual' => array('id' => '_virtual', 'wrapper_class' => 'show_if_simple', 'label' => __( 'Virtual', 'wcvendors-pro' ), 'description'   => __( 'Virtual products are intangible and aren\'t shipped.', 'wcvendors-pro' ), 'default'       => 'no')
-  			);
-  	return $product_type_options;
-  }
+    // $args[ 'custom_attributes' ] = array(
+    //   'post_id'	=> $post_id,
+    //   'id' 		=> 'post_content',
+    //   'label'	 	=> __( 'Product Description', 'wcvendors-pro' ),
+    //   'value' 	=> $product_description,
+    //   'placeholder' 		=> __( 'Please add a full description of your product here', 'wcvendors-pro' ),
+    //   'custom_attributes' => array(
+    //     'data-rules' => 'required',
+    //     'data-error' => __( 'Product description is required.', 'wcvendors-pro' )
+    //
+    //   ));
+
+      // 'wcv_product_title', array(
+      //  'post_id' 			=> $post_id,
+      //  'id'	 			=> 'post_title',
+      //  'label' 			=> __( 'Product Name', 'wcvendors-pro' ),
+      //  'value' 			=> $product_title,
+      //  'custom_attributes' => array(
+      //      'data-rules' => 'required|max_length[100]',
+      //      'data-error' => __( 'Product name is required or is too long.', 'wcvendors-pro' ),
+      //      'data-label' => __( 'Product Name', 'wcvendors-pro' ),
+      //
+      //    )
+
+
+      // 'wcv_product_categories',
+      //   array(
+      //     'post_id'			=> $post_id,
+      //     'id' 				=> 'product_cat[]',
+      //     'taxonomy'			=> 'product_cat',
+      //     'show_option_none'	=> $show_option_none,
+      //     'taxonomy_args'		=> array(
+      //                 'hide_empty'	=> 0,
+      //                 'orderby'		=> 'order',
+      //                 'exclude'		=> $exclude,
+      //               ),
+      //     'label'	 			=> ( $multiple ) ? __( 'Categories', 'wcvendors-pro' ) : __( 'Category', 'wcvendors-pro' ),
+      //     'custom_attributes' => $custom_attributes,
+      //     )
+      //   )
+
+
+        //
+        // WCVendors_Pro_Form_Helper::input( apply_filters( 'wcv_product_tags', array(
+        //     'id' 					=> 'product_tags',
+        //     'label' 				=> __( 'Tags', 'wcvendors-pro' ),
+        //     'value' 				=> implode( ',', array_keys( $tag_ids ) ),
+        //     'style'					=> 'width: 100%;',
+        //     'class'					=> 'wcv-tag-search',
+        //     'type'					=> 'hidden',
+        //     'show_label'			=> 'true',
+        //     'custom_attributes' 	=> array(
+        //         'data-placeholder' 	=> __( 'Search or add a tag&hellip;', 'wcvendors-pro' ),
+        //         'data-action'		=> 'wcv_json_search_tags',
+        //         'data-multiple' 	=> 'true',
+        //         'data-tags'			=> 'true',
+        //         'data-selected'		=> esc_attr( json_encode( $tag_ids ) )
+        //       ),
+        //   ) )
+        // );
+
+        // WCVendors_Pro_Form_Helper::input( apply_filters( 'wcv_product_price', array(
+        //   'post_id'		=> $post_id,
+        //   'id' 			=> '_regular_price',
+        //   'label' 		=> __( 'Regular Price', 'wcvendors-pro' ) . ' (' . get_woocommerce_currency_symbol() . ')',
+        //   'data_type' 	=> 'price',
+        //   'wrapper_start' => $wrapper_start,
+        //   'wrapper_end' 	=> '</div>',
+        //   'custom_attributes' => array(
+        //     'data-rules' => 'decimal',
+        //     'data-error' => __( 'Price should be a number.', 'wcvendors-pro' )
+        //
+        //   )
+
+
+
+      // 'wcv_product_sale_price', array(
+			// 	'post_id'		=> $post_id,
+			// 	'id' 			=> '_sale_price',
+			// 	'data_type' 	=> 'price',
+			// 	'label' 		=> __( 'Sale Price', 'wcvendors-pro' ) . ' ('.get_woocommerce_currency_symbol().')',
+			// 	'desc_tip' 		=> 'true',
+			// 	'description' 	=> '<a href="#" class="sale_schedule right">' . __( 'Schedule', 'wcvendors-pro' ) . '</a>',
+			// 	'wrapper_start' => '<div class="all-50 small-100">',
+			// 	'wrapper_end' 	=>  '</div></div>',
+			// 	'custom_attributes' => array(
+		 // 			'data-rules' => 'decimal',
+		 // 			'data-error' => __( 'Sale price should be a number.', 'wcvendors-pro' )
+      //
+      //
+
+
+
+}
+
+// Using description as an example
+
+
+
 
 
   private function custom_user_login_redirect( $redirect_to, $request, $user ) {
--- content/themes/rehub-vendor/wc-vendors/dashboard/product-edit.php
+++ content/themes/rehub-vendor/wc-vendors/dashboard/product-edit.php
@@ -43,170 +43,39 @@ $post_status				= ( isset($product) && null !== $product ) ? $product->post->pos
 	    <?php WCVendors_Pro_Product_Form::categories( $object_id, true ); ?>
 	    <!-- Product Tags -->
 	    <?php WCVendors_Pro_Product_Form::tags( $object_id, true ); ?>
+			<p class="tip">Virgül ile ayır ör: a,b,c,d</p>
+			<br/>
 	</div>
 
 	<div class="all-100">
-    	<!-- Media uploader -->
-		<div class="wcv-product-media">
-			<?php do_action( 'wcv_before_media', $object_id ); ?>
-				<?php WCVendors_Pro_Form_helper::product_media_uploader( $object_id ); ?>
-			<?php do_action( 'wcv_after_media', $object_id ); ?>
-
+		<label for="wcv_product_attributes" class=""><?php _e( 'Attributes', 'woocommerce' ); ?></label>
+		<!-- Attributes -->
+		<?php do_action( 'wcv_before_attributes_tab', $object_id ); ?>
+		<div class="wcv_product_attributes">
+			<?php WCVendors_Pro_Product_Form::product_attributes( $object_id ); ?>
 		</div>
-	</div>
 
-	<hr />
+		<?php do_action( 'wcv_after_attributes_tab', $object_id ); ?>
 
-	<div class="all-100">
-		<!-- Product Type -->
-		<div class="wcv-product-type hidden">
-			<?php WCVendors_Pro_Product_Form::product_type( $object_id ); ?>
+		<!-- Price and Sale Price -->
+		<div class="show_if_simple show_if_external">
+			<?php WCVendors_Pro_Product_Form::prices( $object_id ); ?>
 		</div>
-	</div>
 
-	<div class="all-100">
-		<div class="wcv-tabs top" data-prevent-url-change="true">
-
-			<?php WCVendors_Pro_Product_Form::product_meta_tabs( ); ?>
-
-			<?php do_action( 'wcv_before_general_tab', $object_id ); ?>
-
-			<!-- General Product Options -->
-			<div class="wcv-product-general tabs-content" id="general">
-
-				<div class="hide_if_grouped">
-					<!-- SKU  -->
-					<?php WCVendors_Pro_Product_Form::sku( $object_id ); ?>
-					<!-- Private listing  -->
-					<?php WCVendors_Pro_Product_Form::private_listing( $object_id ); ?>
-				</div>
-
-
-				<div class="options_group show_if_external">
-					<?php WCVendors_Pro_Product_Form::external_url( $object_id ); ?>
-					<?php WCVendors_Pro_Product_Form::button_text( $object_id ); ?>
-				</div>
-
-				<div class="show_if_simple show_if_external">
-					<!-- Price and Sale Price -->
-					<?php WCVendors_Pro_Product_Form::prices( $object_id ); ?>
-				</div>
-
-				<div class="show_if_simple show_if_external show_if_variable">
-					<!-- Tax -->
-					<?php WCVendors_Pro_Product_Form::tax( $object_id ); ?>
-				</div>
-
-				<div class="show_if_downloadable" id="files_download">
-					<!-- Downloadable files -->
-					<?php WCVendors_Pro_Product_Form::download_files( $object_id ); ?>
-					<!-- Download Limit -->
-					<?php WCVendors_Pro_Product_Form::download_limit( $object_id ); ?>
-					<!-- Download Expiry -->
-					<?php WCVendors_Pro_Product_Form::download_expiry( $object_id ); ?>
-					<!-- Download Type -->
-					<?php WCVendors_Pro_Product_Form::download_type( $object_id ); ?>
-				</div>
-			</div>
-
-			<?php do_action( 'wcv_after_general_tab', $object_id ); ?>
-
-			<?php do_action( 'wcv_before_inventory_tab', $object_id ); ?>
-
-			<!-- Inventory -->
-			<div class="wcv-product-inventory inventory_product_data tabs-content" id="inventory">
-
-				<?php WCVendors_Pro_Product_Form::manage_stock( $object_id ); ?>
-
-				<?php do_action( 'wcv_product_options_stock' ); ?>
-
-				<div class="stock_fields show_if_simple show_if_variable">
-					<?php WCVendors_Pro_Product_Form::stock_qty( $object_id ); ?>
-					<?php WCVendors_Pro_Product_Form::backorders( $object_id ); ?>
-				</div>
-
-				<?php WCVendors_Pro_Product_Form::stock_status( $object_id ); ?>
-				<div class="options_group show_if_simple show_if_variable">
-					<?php WCVendors_Pro_Product_Form::sold_individually( $object_id ); ?>
-				</div>
-
-				<?php do_action( 'wcv_product_options_sold_individually' ); ?>
-
-				<?php do_action( 'wcv_product_options_inventory_product_data' ); ?>
+		<div class="all-100">
+	    	<!-- Media uploader -->
+			<div class="wcv-product-media">
+				<?php do_action( 'wcv_before_media', $object_id ); ?>
+					<?php WCVendors_Pro_Form_helper::product_media_uploader( $object_id ); ?>
+				<?php do_action( 'wcv_after_media', $object_id ); ?>
 
 			</div>
+		</div>
 
-			<?php do_action( 'wcv_after_inventory_tab', $object_id ); ?>
+		<?php WCVendors_Pro_Product_Form::form_data( $object_id, $post_status ); ?>
+		<?php WCVendors_Pro_Product_Form::save_button( $title ); ?>
+		<?php WCVendors_Pro_Product_Form::draft_button( __('Save Draft','wcvendors-pro') ); ?>
 
-			<?php do_action( 'wcv_before_shipping_tab', $object_id ); ?>
 
-			<!-- Shipping  -->
-			<div class="wcv-product-shipping shipping_product_data tabs-content" id="shipping">
-
-				<div class="hide_if_grouped hide_if_external">
-
-					<!-- Shipping rates  -->
-					<?php WCVendors_Pro_Product_Form::shipping_rates( $object_id ); ?>
-					<!-- weight  -->
-					<?php WCVendors_Pro_Product_Form::weight( $object_id ); ?>
-					<!-- Dimensions -->
-					<?php WCVendors_Pro_Product_Form::dimensions( $object_id ); ?>
-					<?php do_action( 'wcv_product_options_dimensions' ); ?>
-					<!-- shipping class -->
-					<?php WCVendors_Pro_Product_Form::shipping_class( $object_id ); ?>
-					<?php do_action( 'wcv_product_options_shipping' ); ?>
-				</div>
-
-			</div>
-
-			<?php do_action( 'wcv_after_shipping_tab', $object_id ); ?>
-
-			<?php do_action( 'wcv_before_linked_tab', $object_id ); ?>
-
-			<!-- Upsells and grouping -->
-			<div class="wcv-product-upsells tabs-content" id="linked_product">
-
-				<?php WCVendors_Pro_Product_Form::up_sells( $object_id ); ?>
-
-				<?php WCVendors_Pro_Product_Form::crosssells( $object_id ); ?>
-
-				<div class="hide_if_grouped hide_if_external">
-
-					<?php WCVendors_Pro_Product_Form::grouped_products( $object_id, $product ); ?>
-
-				</div>
-			</div>
-
-			<?php do_action( 'wcv_after_linked_tab', $object_id ); ?>
-
-			<!-- Attributes -->
-
-			<?php do_action( 'wcv_before_attributes_tab', $object_id ); ?>
-
-			<div class="wcv_product_attributes tabs-content" id="attributes">
-
-				<?php WCVendors_Pro_Product_Form::product_attributes( $object_id ); ?>
-
-			</div>
-
-			<?php do_action( 'wcv_after_attributes_tab', $object_id ); ?>
-
-			<!-- Variations -->
-
-			<?php do_action( 'wcv_before_variations_tab', $object_id ); ?>
-
-			<div class="wcv_product_variations tabs-content" id="variations">
-
-				<?php WCVendors_Pro_Product_Form::product_variations( $object_id ); ?>
-
-			</div>
-
-			<?php do_action( 'wcv_after_variations_tab', $object_id ); ?>
-
-			<?php WCVendors_Pro_Product_Form::form_data( $object_id, $post_status ); ?>
-			<?php WCVendors_Pro_Product_Form::save_button( $title ); ?>
-			<?php WCVendors_Pro_Product_Form::draft_button( __('Save Draft','wcvendors-pro') ); ?>
-
-			</div>
 		</div>
 </form>
